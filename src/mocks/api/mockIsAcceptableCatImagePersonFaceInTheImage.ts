95bcb6835fa9704a6beb96d32103f41f5439e4ad
keitakn <keita.koga.work@gmail.com>
2023-01-01 21:26:39 +0900

    :white_check_mark: #7 異常系のテストケースを追加

---
+++ src/mocks/api/mockIsAcceptableCatImagePersonFaceInTheImage.ts
@@ -0,0 +1,18 @@
+import { ResponseResolver, MockedRequest, restContext } from 'msw';
+
+import { httpStatusCode } from '../../httpStatusCode';
+
+export const mockIsAcceptableCatImagePersonFaceInTheImage: ResponseResolver<
+  MockedRequest,
+  typeof restContext
+> = (_req, res, ctx) =>
+  res(
+    ctx.status(httpStatusCode.ok),
+    ctx.set('Content-Type', 'application/json'),
+    ctx.set('X-request-Id', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'),
+    ctx.set('X-lambda-request-Id', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'),
+    ctx.json({
+      isAcceptableCatImage: false,
+      notAcceptableReason: 'person face in the image',
+    })
+  );
