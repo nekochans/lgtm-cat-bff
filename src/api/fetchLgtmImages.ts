f3a100bc77c1f8e2b03d7cb6b27563a69f71d22e
keitakn <keita.koga.work@gmail.com>
2023-01-04 13:08:11 +0900

    :sparkles: #6 最新のLGTM画像を取得する関数を実装

--- src/api/fetchLgtmImages.ts
+++ src/api/fetchLgtmImages.ts
@@ -1,6 +1,7 @@
 import type { LgtmImage } from '../lgtmImage';
 import { createFailureResult, createSuccessResult, Result } from '../result';
 import { FetchLgtmImagesInRandomError } from './errors/FetchLgtmImagesInRandomError';
+import { FetchLgtmImagesInRecentlyCreated } from './errors/FetchLgtmImagesInRecentlyCreated';
 import {
   isFetchLgtmImagesResponseBody,
   validateFetchLgtmImagesResponseBody,
@@ -50,6 +51,64 @@ export const fetchLgtmImagesInRandom = async (
     const requestIds = mightExtractRequestIds(response);
 
     throw new FetchLgtmImagesInRandomError(
+      `X-Request-Id=${String(
+        requestIds.xRequestId
+      )}:X-Lambda-Request-Id:${String(requestIds.xLambdaRequestId)}`
+    );
+  }
+
+  const responseBody = await response.json();
+  if (isFetchLgtmImagesResponseBody(responseBody)) {
+    const successResponse: SuccessResponse = {
+      lgtmImages: responseBody.lgtmImages.map((value) => {
+        return { id: Number(value.id), imageUrl: value.url };
+      }),
+    };
+
+    const requestIds = mightExtractRequestIds(response);
+
+    return createSuccessResult<SuccessResponse>({
+      ...successResponse,
+      ...requestIds,
+    });
+  }
+
+  const validationResult = validateFetchLgtmImagesResponseBody(responseBody);
+  if (!validationResult.isValidate && validationResult.invalidParams != null) {
+    return createFailureResult<ValidationErrorResponse>(
+      createValidationErrorResponse(validationResult.invalidParams, response)
+    );
+  }
+
+  const requestIds = mightExtractRequestIds(response);
+
+  throw new FetchLgtmImagesInRandomError(
+    `X-Request-Id=${String(requestIds.xRequestId)}:X-Lambda-Request-Id:${String(
+      requestIds.xLambdaRequestId
+    )}`
+  );
+};
+
+export const fetchLgtmImagesInRecentlyCreated = async (
+  dto: Dto
+): Promise<
+  Result<SuccessResponse, FailureResponse | ValidationErrorResponse>
+> => {
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${dto.accessToken}`,
+    },
+  };
+
+  const response = await fetch(
+    `${dto.apiBaseUrl}/lgtm-images/recently-created`,
+    options
+  );
+  if (!response.ok) {
+    const requestIds = mightExtractRequestIds(response);
+
+    throw new FetchLgtmImagesInRecentlyCreated(
       `X-Request-Id=${String(
         requestIds.xRequestId
       )}:X-Lambda-Request-Id:${String(requestIds.xLambdaRequestId)}`
